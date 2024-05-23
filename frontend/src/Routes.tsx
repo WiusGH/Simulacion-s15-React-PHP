4f62ea501a2a3a1873a34a6ac224b01aa5c00da2
Veritoa45 <molinariveronica478@gmail.com>
2024-05-23 17:08:39 -0300

    feat: Agregar el Tateti

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
@@ -6,18 +5,8 @@ import SignUp from "./pages/SignUp";
 import Search from "./pages/Search";
 import Categories from "./pages/Categories";
 import Favorites from "./pages/Favorites";
-=======
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Search from './pages/Search';
-import Categories from './pages/Categories';
-import Favorites from './pages/Favorites';
-import GameViewer from './pages/GameViewer';
-import Error404 from './pages/Error404';
->>>>>>> origin/main
+import GameViewer from "./pages/GameViewer";
+import Error404 from "./pages/Error404";
 
 const Routess: React.FC = () => {
   return (
@@ -28,7 +17,8 @@ const Routess: React.FC = () => {
         <Route path="/buscar" element={<Search />} />
         <Route path="/categorias" element={<Categories />} />
         <Route path="/favoritos" element={<Favorites />} />
-        <Route path="/jugar/:game" element={<GameViewer />} /> {/* Monta dinámicamente un juego según el URL introducido */}
+        <Route path="/jugar/:game" element={<GameViewer />} />{" "}
+        {/* Monta dinámicamente un juego según el URL introducido */}
         <Route path="*" element={<Error404 />} />
       </Routes>
     </Router>
--- frontend/src/pages/GameViewer.tsx
+++ frontend/src/pages/GameViewer.tsx
@@ -5,15 +5,15 @@ import Footer from "../components/footer/Footer";
 import MainContainer from "../components/containers/MainContainer";
 import Error404 from "./Error404";
 import Simon from "../components/games/Simon";
+import Tateti from "../components/games/Tateti";
 // Importar otros juegos a medida que se vayan creando
 
-
 const GameViewer: React.FC = () => {
-
   // Obtiene el nombre del juego de la url
   const { game } = useParams<{ game: string }>();
   const games: { [key: string]: JSX.Element } = {
     simon: <Simon />,
+    tateti: <Tateti />,
     // Colocar acá los juegos que se vayan agregando
   };
 
@@ -27,7 +27,8 @@ const GameViewer: React.FC = () => {
   return (
     <div>
       <Header />
-      <MainContainer game={selectedGame} /> {/* Muestra dinámicamente el juego seleccionado */}
+      <MainContainer game={selectedGame} />{" "}
+      {/* Muestra dinámicamente el juego seleccionado */}
       {/* TODO: agregar sidebar */}
       <Footer />
     </div>
